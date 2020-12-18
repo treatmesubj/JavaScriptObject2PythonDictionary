59a7c5ae8529244de222ab940da881598da04a4a
John Hupperts <39680353+treatmesubj@users.noreply.github.com>
2020-12-18 15:01:06 -0600

    less moronic regex

--- jsO2D.py
+++ jsO2D.py
@@ -1,7 +1,7 @@
 import re
 import regex  # conditional look-arounds
 import json
-# legit regex: (?<=var(.|\n)*?javascript_object_name(.|\n)*?=(.|\n)*?)(?<!var(.|\n)*?javascript_object_name(.|\n)*?=(.|\n)*?;(.|\n)*?)({((.|\n)*?)});
+# legit regex: (?<=javascript_object_name\s*=\s*){.+?}(?=;)
 
 
 def js_obj_from_script_to_dict(javascript_object_name, script):
@@ -21,8 +21,21 @@ def js_obj_from_script_to_dict(javascript_object_name, script):
 		        return match.group(1)
 		return regex.sub(_replacer, string)
 
-
-	reggy = fr"(?<=var(.|\n)*?{javascript_object_name}(.|\n)*?=(.|\n)*?)(?<!var(.|\n)*?{javascript_object_name}(.|\n)*?=(.|\n)*?;(.|\n)*?)({{((.|\n)*?)}});"
+	reggy = fr"(?<={javascript_object_name}\s*=\s*){{.+?}}(?=;)"
 	jaysean = regex.search(reggy, _remove_comments(script))
-	sanjay = json.loads(jaysean.group(8))
+	sanjay = json.loads(jaysean.group())
 	return sanjay
+
+
+# def test():
+
+# 	import requests
+# 	from bs4 import BeautifulSoup
+
+# 	url = "https://www.youtube.com/feed/storefront"
+# 	response = requests.get(url)
+# 	soup = BeautifulSoup(response.text, 'html.parser')
+# 	pattern = re.compile(r"ytInitialData\s=\s({.*});")
+# 	script_elem = soup.find("script", text=pattern)
+# 	dicty = js_obj_from_script_to_dict("ytInitialData", script_elem.string)
+# 	print(list(dicty.keys()))
